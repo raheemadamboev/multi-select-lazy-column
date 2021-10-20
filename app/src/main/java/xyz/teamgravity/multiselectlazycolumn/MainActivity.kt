437a61e83777a9518a9d54822fe589009c5e1265
Raheem <raheemideadev@gmail.com>
2021-10-20 23:15:11 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/multiselectlazycolumn/MainActivity.kt
@@ -0,0 +1,20 @@
+package xyz.teamgravity.multiselectlazycolumn
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import xyz.teamgravity.multiselectlazycolumn.ui.theme.MultiSelectLazyColumnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
